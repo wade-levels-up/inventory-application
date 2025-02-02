7411406fc080cf257291dc0b6d36edfdae4bda8c
Wade Foster <>
2025-02-02 20:38:17 +1100

    Validate and sanitize manufacturer form inputs

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,14 +1,28 @@
 const db = require('../db/queries');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator')
 
-const createNewManufacturer = asyncHandler(async (req, res) => {
-    try {
-        await db.createNewManufacturer(req.body.name);
-        res.redirect('/')
-    } catch(error) {
-        throw new Error(`Unable to create new manufacturer - ${error}`);
-    }
-})
+const newManufacturerValidator = [
+    body('name').trim(),
+    body('name', 'Cannot be empty').notEmpty().withMessage('Name cannot be empty'),
+    body('name', 'Cannot contain numbers').isAlpha().withMessage('Name cannot contain numbers'),
+    body('name', 'Meets length requirements').isLength({ min: 1, max: 30 }).withMessage(`Name cannot be less than 1 character long or greater than 30 characters long`)
+]
+
+const createNewManufacturer = [
+    newManufacturerValidator,
+    asyncHandler(async (req, res) => {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).render("pages/new", { title: 'New Manufacturer', errors: errors.array()})
+            }
+            await db.createNewManufacturer(req.body.name);
+            res.redirect('/')
+        } catch(error) {
+            throw new Error(`Unable to create new manufacturer - ${error}`);
+        }
+})]
 
 const getNewManufacturerForm = asyncHandler(async (req, res) => {
     try {
